b2c52eed6bd245fb774cd4a27d98e9297bd336bd
KevinH45 <timmyhwang127@gmail.com>
2024-06-24 13:26:45 -0400

    fixed header and changed footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -17,11 +17,24 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by the Glenelg High School Competitive Programming Team <br></br> 
-        Coded by {""}
+        Made With ❤ by {""}
+        <Link href="https://www.linkedin.com/in/kevin-hwang-8a7627219/">
+          <a className="underline underline-offset-1">Kevin Hwang</a>
+        </Link>
+        <p style={{'display': 'inline'}}> and </p>
         <Link href="https://www.linkedin.com/in/kenneth-wang-854606258/">
           <a className="underline underline-offset-1">Kenneth Wang</a>
         </Link>
+
+        <br></br>
+        <br></br>
+        
+        All donations to the club are tax deductible. Donate {""} 
+        <Link href="https://hcb.hackclub.com/donations/start/glenelg-hs-competitive-programming-club">
+          <a className="underline underline-offset-1">here </a>
+        </Link>
+        today!
+
       </h1>
     </>
   );
